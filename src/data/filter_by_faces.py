78009104d34b16278a1cf86df45084cbbc6c216e
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-02-09 17:08:02 +0100

    Add argparse for flags when running from shell #4

--- src/data/filter_by_faces.py
+++ src/data/filter_by_faces.py
@@ -1,1 +1,10 @@
-import os
+import argparse
+parser = argparse.ArgumentParser()
+
+# python src/data/filter_by_faces.py -src data/raw/ -out data/filtered/
+parser.add_argument("-src", "--source", help="Source directory")
+parser.add_argument("-out", "--output", help="Output directory")
+
+args = parser.parse_args()
+
+print(f"Source directory {args.source} Output directory {args.output}")
