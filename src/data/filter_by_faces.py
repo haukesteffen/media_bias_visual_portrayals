b89895d3f232937aec5480d31a1e8aff9e9dfe18
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-02-09 17:21:22 +0100

    Add os.listdir to list files in source directory #4

--- src/data/filter_by_faces.py
+++ src/data/filter_by_faces.py
@@ -1,10 +1,18 @@
-import argparse
-parser = argparse.ArgumentParser()
-
 # python src/data/filter_by_faces.py -src data/raw/ -out data/filtered/
+# imports 
+import argparse
+import os
+
+# setup argparser, add and parse arguments
+parser = argparse.ArgumentParser()
 parser.add_argument("-src", "--source", help="Source directory")
 parser.add_argument("-out", "--output", help="Output directory")
-
 args = parser.parse_args()
+source_directory = args.source
+output_directory = args.output
 
-print(f"Source directory {args.source} Output directory {args.output}")
+
+dir_list = os.listdir(source_directory)
+
+
+print(f"Source directory {args.source}\nOutput directory {args.output}\nFiles found in source directory: {dir_list}")
