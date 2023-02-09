ecf9bb8a4b5a9c70b1157aa1795cb3aecd79b550
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-02-09 18:45:45 +0100

    Add shebang, sanity checks for directories, jpg filter for file list #4

--- src/data/filter_by_faces.py
+++ src/data/filter_by_faces.py
@@ -1,4 +1,6 @@
-# python src/data/filter_by_faces.py -src data/raw/ -out data/filtered/
+#!/usr/bin/env python
+
+# ./filter_by_faces.py -src data/raw/ -out data/filtered/
 # imports 
 import argparse
 import os
@@ -11,8 +13,13 @@ args = parser.parse_args()
 source_directory = args.source
 output_directory = args.output
 
+# assert directories exist and are not the same
+assert os.path.isdir(source_directory), print('Source directory does not exist')
+assert os.path.isdir(output_directory), print('Output directory does not exist')
+assert source_directory != output_directory, print('Source and output can not be the same directory')
 
+# create list of files in source directory and filter for .jpg files
 dir_list = os.listdir(source_directory)
+img_list = [img for img in dir_list if img.endswith('.jpg')]
 
-
-print(f"Source directory {args.source}\nOutput directory {args.output}\nFiles found in source directory: {dir_list}")
+print(f"Source directory {args.source}\nOutput directory {args.output}\nImages found in source directory: {img_list}")
