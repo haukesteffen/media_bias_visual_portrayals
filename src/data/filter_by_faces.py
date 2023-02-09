ed28150ef9e51bd93c5d1d9675f2e4c93c07d436
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-02-09 18:55:16 +0100

    Add functions #4

--- src/data/filter_by_faces.py
+++ src/data/filter_by_faces.py
@@ -5,21 +5,37 @@
 import argparse
 import os
 
-# setup argparser, add and parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-src", "--source", help="Source directory")
-parser.add_argument("-out", "--output", help="Output directory")
-args = parser.parse_args()
-source_directory = args.source
-output_directory = args.output
+def get_flags():
+    # setup argparser, add and parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-src", "--source", help="Source directory")
+    parser.add_argument("-out", "--output", help="Output directory")
+    args = parser.parse_args()
+    src = args.source
+    out = args.output
 
-# assert directories exist and are not the same
-assert os.path.isdir(source_directory), print('Source directory does not exist')
-assert os.path.isdir(output_directory), print('Output directory does not exist')
-assert source_directory != output_directory, print('Source and output can not be the same directory')
+    # assert directories exist and are not the same
+    assert os.path.isdir(src), print('Source directory does not exist')
+    assert os.path.isdir(out), print('Output directory does not exist')
+    assert src != out, print('Source and output can not be the same directory')
 
-# create list of files in source directory and filter for .jpg files
-dir_list = os.listdir(source_directory)
-img_list = [img for img in dir_list if img.endswith('.jpg')]
+    return src, out
 
-print(f"Source directory {args.source}\nOutput directory {args.output}\nImages found in source directory: {img_list}")
+def list_images(dir):
+    # returns a list of all images with .jpg type in input directory
+    dir_list = os.listdir(dir)
+    img_list = [img for img in dir_list if img.endswith('.jpg')]
+    return img_list
+
+def main():
+    # get source and output directories from shell flags
+    source_directory, output_directory = get_flags()
+
+    # create list of files in source directory and filter for .jpg files
+    img_list = list_images(source_directory)
+
+    print(f"Source directory {source_directory}\nOutput directory {output_directory}\nImages found in source directory: {img_list}")
+
+
+if __name__ == "__main__":
+    main()
