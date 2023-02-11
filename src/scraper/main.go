bdc1fafcb5b34d4ce3c787b52296fe5b5ba60e49
jan_s+git@posteo.de <jan_s+github@posteo.de>
2023-02-11 22:47:40 +0100

    Parse args

--- src/scraper/main.go
+++ src/scraper/main.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
+	"path"
+	"strings"
 	"time"
 
 	customsearch "google.golang.org/api/customsearch/v1"
@@ -19,14 +21,15 @@ import (
 type SearchConfig struct {
 	apiKey       string
 	cx           string
-	query        string
+	folder       string
 	searchType   string
-	searchSites  []string
+	searchSite   string
 	searchPerson string
 }
 
 func newConfig() *SearchConfig {
 	c := &SearchConfig{}
+	// Secret data from env vars
 	val, err := os.LookupEnv("SEARCHAPIKEY")
 	if !err {
 		fmt.Fprintln(os.Stderr, "Set SEARCHAPIKEY env")
@@ -39,31 +42,28 @@ func newConfig() *SearchConfig {
 		os.Exit(1)
 	}
 	c.cx = val
-	val, err = os.LookupEnv("SEARCHQUERY")
-	if !err {
-		fmt.Fprintln(os.Stderr, "Set SEARCHQUERY env")
-		os.Exit(1)
+	// ... search data from args
+	flag.StringVar(&c.searchPerson, "person", "", "Person to search for")
+	flag.StringVar(&c.searchSite, "site", "", "Site to use")
+	flag.StringVar(&c.folder, "dir", "/data/", "Directory to save pictures to")
+	needs_help := flag.Bool("help", false, "Show help")
+	flag.Parse()
+	if *needs_help {
+		flag.Usage()
+		os.Exit(0)
 	}
-	c.query = val
-	// todo: vars
+	if len(c.searchPerson) == 0 || len(c.searchSite) == 0 {
+		log.Fatal("Please provide -person and -site arguments")
+	}
 	c.searchType = "image"
-	c.searchSites = []string{"tagesschau.de", "faz.net", "welt.de", "bild.de"}
-	c.searchPerson = "erich honecker"
 	return c
 }
 
-func queryBuilder(svc *customsearch.Service, conf *SearchConfig) *customsearch.CseListCall {
+func queryBuilder(svc *customsearch.Service, conf *SearchConfig, startIndex int64) *customsearch.CseListCall {
 	// todo: defaults auslagern
-	lst := svc.Cse.List().Cx(conf.cx).SearchType(conf.searchType).Sort("date")
+	lst := svc.Cse.List().Cx(conf.cx).SearchType(conf.searchType).Sort("date:d:s").Num(10).Start(1 + startIndex*10).ImgType("face")
 	query := ""
-	// todo: maybe use 'orTerms' here
-	for i, site := range conf.searchSites {
-		query += "site:" + site
-		// Dont add 'OR' keyword at the end
-		if i != len(conf.searchSites)-1 {
-			query += " OR "
-		}
-	}
+	query += "site:" + conf.searchSite
 	query += " " + conf.searchPerson
 	lst.Q(query)
 	log.Println("debug searc: ", *lst)
@@ -87,7 +87,7 @@ func downloadFile(URL string) ([]byte, error) {
 	return data.Bytes(), nil
 }
 
-func fetchAndSaveImage(urls []string) error {
+func fetchAndSaveImage(urls []string, conf *SearchConfig) error {
 	done := make(chan []byte, len(urls))
 	errch := make(chan error, len(urls))
 	for _, URL := range urls {
@@ -103,13 +103,14 @@ func fetchAndSaveImage(urls []string) error {
 		}(URL)
 	}
 
-	rand.Seed(time.Now().UnixNano())
 	var errStr string
+	picNamesBase := path.Join(conf.folder, strings.ReplaceAll(conf.searchPerson, " ", "-")+"_"+conf.searchSite+"_")
 	for i := 0; i < len(urls); i++ {
 		if err := <-errch; err != nil {
 			errStr = errStr + " " + err.Error()
 		}
-		file, err := os.Create("./data/asd" + fmt.Sprint(rand.Intn(1000000)))
+		now := time.Now().UnixNano()
+		file, err := os.Create(picNamesBase + fmt.Sprint(now) + ".jpg")
 		if err != nil {
 			return err
 		}
@@ -133,18 +134,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	svcQuery := queryBuilder(svc, conf)
+	for i := 0; i < 1; i++ {
+		svcQuery := queryBuilder(svc, conf, int64(i))
 
-	resp, err := svcQuery.Do()
-	if err != nil {
-		log.Fatal(err)
-	}
+		resp, err := svcQuery.Do()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	urlList := []string{}
-	for i, result := range resp.Items {
-		fmt.Printf("#%d: %s\n", i+1, result.Title)
-		fmt.Printf("\t%s\n", result.Link)
-		urlList = append(urlList, result.Link)
+		urlList := []string{}
+		for _, result := range resp.Items {
+			//fmt.Printf("#%d: %s\n", i+1, result.Title)
+			//fmt.Printf("\t%s\n", result.Link)
+			urlList = append(urlList, result.Link)
+		}
+		fetchAndSaveImage(urlList, conf)
 	}
-	fetchAndSaveImage(urlList)
 }
